1506f55d1fd14e38a498d2b361917b56646f3695
Derek Leverenz <derek@yurts.ai>
2024-11-13 16:49:22 -0800

    add logging and an extra case to postgres example

--- examples/postgres.rs
+++ examples/postgres.rs
@@ -1,38 +1,55 @@
-use datafusion::prelude::SessionContext;
-use datafusion::sql::TableReference;
-use datafusion_table_providers::{
-    common::DatabaseCatalogProvider, postgres::PostgresTableFactory,
-    sql::db_connection_pool::postgrespool::PostgresConnectionPool, util::secrets::to_secret_map,
-};
-use std::collections::HashMap;
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
-/// This example demonstrates how to:
-/// 1. Create a PostgreSQL connection pool
-/// 2. Create and use PostgresTableFactory to generate TableProvider
-/// 3. Register TableProvider with DataFusion
-/// 4. Use SQL queries to access PostgreSQL table data
+use datafusion::{prelude::SessionContext, sql::TableReference};
+use datafusion_table_providers::{
+    postgres::PostgresTableFactory, sql::db_connection_pool::postgrespool::PostgresConnectionPool,
+    util::secrets::to_secret_map,
+};
+
+use tracing_subscriber::{
+    filter::{EnvFilter, LevelFilter},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+
+/// Initialize tracing-based log exporter
+pub fn init_logging() {
+    // use pretty logs for now. Later we can make an alternate layer that will emit
+    // json logs, for running as a live service
+    let loglayer = tracing_subscriber::fmt::layer().pretty();
+
+    let subscriber = tracing_subscriber::registry().with(loglayer).with(
+        EnvFilter::builder()
+            .with_default_directive(LevelFilter::INFO.into())
+            .from_env_lossy(),
+    );
+    subscriber.try_init().ok();
+}
+
+/// This example demonstrates how to register a table provider into DataFusion that
+/// uses a Postgres table as its source.
 ///
-/// Prerequisites:
-/// Start a PostgreSQL server using Docker:
+/// Use docker to start a Postgres server this example can connect to:
+///
 /// ```bash
 /// docker run --name postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres_db -p 5432:5432 -d postgres:16-alpine
 /// # Wait for the Postgres server to start
 /// sleep 30
 ///
-/// # Create a table and insert sample data
-/// docker exec -i postgres psql -U postgres test_db <<EOF
+/// # Create a table in the Postgres server and insert some data
+/// docker exec -i postgres psql -U postgres -d postgres_db <<EOF
 /// CREATE TABLE companies (
-///    id SERIAL PRIMARY KEY,
-///    name VARCHAR(100)
+///    id INT PRIMARY KEY,
+///   name VARCHAR(100)
 /// );
 ///
-/// INSERT INTO companies (name) VALUES ('Example Corp');
+/// INSERT INTO companies (id, name) VALUES (1, 'Acme Corporation');
 /// EOF
 /// ```
 #[tokio::main]
 async fn main() {
-    // Create PostgreSQL connection parameters
+    init_logging();
+
     let postgres_params = to_secret_map(HashMap::from([
         ("host".to_string(), "localhost".to_string()),
         ("user".to_string(), "postgres".to_string()),
@@ -42,51 +59,36 @@ async fn main() {
         ("sslmode".to_string(), "disable".to_string()),
     ]));
 
-    // Create PostgreSQL connection pool
     let postgres_pool = Arc::new(
         PostgresConnectionPool::new(postgres_params)
             .await
-            .expect("unable to create PostgreSQL connection pool"),
+            .expect("unable to create Postgres connection pool"),
     );
 
-    // Create PostgreSQL table provider factory
-    // Used to generate TableProvider instances that can read PostgreSQL table data
-    let table_factory = PostgresTableFactory::new(postgres_pool.clone());
+    let table_factory = PostgresTableFactory::new(postgres_pool);
 
-    // Create database catalog provider
-    // This allows us to access tables through catalog structure (catalog.schema.table)
-    let catalog = DatabaseCatalogProvider::try_new(postgres_pool)
+    let companies_table = table_factory
+        .table_provider(TableReference::bare("companies"))
         .await
-        .unwrap();
+        .expect("to create table provider");
 
-    // Create DataFusion session context
     let ctx = SessionContext::new();
-    // Register PostgreSQL catalog, making it accessible via the "postgres" name
-    ctx.register_catalog("postgres", Arc::new(catalog));
 
-    // Demonstrate direct table provider registration
-    // This method registers the table in the default catalog
-    // Here we register the PostgreSQL "companies" table as "companies_v2"
-    ctx.register_table(
-        "companies_v2",
-        table_factory
-            .table_provider(TableReference::bare("companies"))
-            .await
-            .expect("failed to register table provider"),
-    )
-    .expect("failed to register table");
+    // It's not required that the name registed in DataFusion matches the table name in DuckDB.
+    ctx.register_table("companies", companies_table)
+        .expect("to register table");
 
-    // Query Example 1: Query the renamed table through default catalog
     let df = ctx
-        .sql("SELECT * FROM datafusion.public.companies_v2")
+        .sql("SELECT * FROM companies")
         .await
         .expect("select failed");
+
     df.show().await.expect("show failed");
 
-    // Query Example 2: Query the original table through PostgreSQL catalog
     let df = ctx
-        .sql("SELECT * FROM postgres.public.companies")
+        .sql("SELECT name FROM companies")
         .await
         .expect("select failed");
+
     df.show().await.expect("show failed");
 }
